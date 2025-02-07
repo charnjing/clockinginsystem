4b52d1b07953cecf9b90866a1cc718de49c621bb
charnjing <charnjing@gmail.com>
2025-02-07 16:30:03 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html'
+                // Add other assets you want to cache for offline use
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
